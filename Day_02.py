0c651ad2cf11658c9d03914ccf181457ba1a279a
Subha822-hub <suseraj1974@gmail.com>
2024-01-02 08:22:07 +0530

    positve_negative

---
+++ Day_02.py
@@ -0,0 +1,27 @@
+# 1.First Approach
+
+number = int(input("Enter A Number: "))
+if number %2==0:
+    print("Even")
+else:
+    print("Odd")
+
+# 2.Second Approach
+
+number = int(input("Enter A Number: "))
+print("Even" if number%2==0 else "Odd")
+
+# 3.Second Approach
+
+def isEven(number):
+    return not number&1
+
+if __name__ == '__main__':
+    number=int(input("Enter A Number: "))
+    if isEven(number):
+        print("Even")
+    else:
+        print("Odd")
+
+
+
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # DaysofCode
 
 -  Day_01 -- [Code](https://github.com/Subha822-hub/DaysofCode/blob/main/Day_01.py)
+-  Day_02 -- [Code](https://github.com/Subha822-hub/DaysofCode/blob/main/Day_02.py)
